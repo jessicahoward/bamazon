b2f213204fbf57623322fbff3c4f2b271cfc36b1
Jessica Howard <jessicahoward1031@gmail.com>
2018-08-18 16:47:44 -0400

    pulls from db for item purchased and total cost

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,64 +15,110 @@ var connection = mysql.createConnection({
   password: "root",
   database: "bamazon_db"
 });
-
+//on connection
 connection.connect(function(err) {
   if (err) throw err;
 //   console.log("connected as id " + connection.threadId);
 //   afterConnection();
     displayProducts();
 });
-
-// function afterConnection() {
-//   connection.query("SELECT * FROM products", function(err, res) {
-//     if (err) throw err;
-//     console.log(res);
-//     connection.end();
-//   });
-// }
  
-// instantiate
+//create cli-table
 var table = new Table({
     head: ['Product ID', 'Product Name', 'Department', 'Price', 'Stock']
 //   
 });
-  
+//global variables accessable to all functions
+  var id;
+  var numberOf;
+  var total;
+  var cost;
+//   var dbId;
+//   var dbName;
+//   var dbDept;
+//   var dbPrice;
+  var dbQuantity;
 // 
-//display table of products usining table and my sql 
+//query database 
 function displayProducts () {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+            var dbId = res[i].item_id;
+            var dbName = res[i].product_name;
+            var dbDept = res[i].department_name;
+            var dbPrice = res[i].price;
+            dbQuantity = res[i].stock_quantity;
+            //push res into cli-table
+            table.push([dbId, dbName, dbDept, dbPrice, dbQuantity])
         };
+        //display cli-table
         console.log(table.toString());
+        //prompt user for input
         inquirer
   .prompt([
     {
         type: "input",
         name: "id",
-        message: "Please enter the ID of the product you would like to purchase: "
+        message: "Please enter the ID of the product you would like to purchase?"
     },
     {
         type: "input",
         name: "quantity",
-        message: "Please enter the quanitity you would like to purchase: " 
+        message: "Please enter the quanitity you would like to purchase?" 
     }
   ]).then(function(answer){
-    console.log(answer.id);
-    console.log(answer.quantity)
-  })
-
-
-       connection.end(); 
+      //reassign variables depending on user response
+      id = answer.id;
+      numberOf = parseInt(answer.quantity);
+      checkStock();
+    // console.log(id);
+    // console.log(numberOf);
+    // console.log(id + numberOf);    
+  })   
     })
+    //end of display products
 }
 
+
+function checkStock () {
+    if (numberOf > parseInt(dbQuantity)) {
+            console.log("Your order exceeds current stock quantity");
+            displayProducts();
+        } else {
+            connection.query("SELECT * FROM products WHERE item_id= ?", [id], function(err, res) {
+                if (err) throw err;
+                var resProduct = res[0];
+                total = parseInt(resProduct.stock_quantity) - numberOf;
+                cost = parseFloat(parseFloat(resProduct.price) * numberOf).toFixed(2);
+                console.log("You have purchased " + 
+                numberOf + 
+                " " + 
+                resProduct + 
+                " for a total price of $" 
+                + cost 
+                + ".");
+            })
+        }
+      connection.end();  
+      //end of check stock
+}
+
+// function updateTable () {
+
+// }
+
+
+// function checkStock () {
+//     connection.query("SELECT * FROM products WHERE id = ?", [id], function (err, res) {
+//         if (err) throw err;
+//         console.log(res);
+//     })
+    
+// }
+
 // 
-//prompt user requesting id of product they would like to buy
-//prompt user asking how many of product
-    //do you have enogh to fil the order?
-        //if not display message "Insufficient quantity!", block the order.
+
         //if yes fufill order
             //update remaining quantity
             //show total cost of purchase
--- seeds.sql
+++ seeds.sql
@@ -1,29 +1,29 @@
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product1", "department1", 999.99, 50);
+VALUES ("The Stranger Beside Me", "Books", 29.99, 25);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product2", "department2", 999.99, 50);
+VALUES ("Red Dragon", "Books", 19.99, 50);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product3", "department3", 999.99, 50);
+VALUES ("Deep Blue Sea", "Movies", 9.99, 6);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product4", "department4", 999.99, 50);
+VALUES ("The Night Manager", "Movies", 999.99, 7);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product5", "department5", 999.99, 50);
+VALUES ("DASH", "Toys", 119.99, 90);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product6", "department6", 999.99, 50);
+VALUES ("Skate Board", "Toys", 79.99, 1000);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product7", "department7", 999.99, 50);
+VALUES ("Bubble Lips", "Cosmetics", 12.99, 17);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product8", "department8", 999.99, 50);
+VALUES ("Transmission Fluid", "Auto Repair", 49.99, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product9", "department9", 999.99, 50);
+VALUES ("Teepee Tent", "Outdoors", 76.99, 23);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product10", "department10", 999.99, 50);
+VALUES ("Camping Stove", "Outdoors", 29.99, 76);
